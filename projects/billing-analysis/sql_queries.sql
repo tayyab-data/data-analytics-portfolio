61424ca185d9cbe3eb4e25ce659464b4d3d869a7
Muhammad Tayyab <muhammadtayyab8838@gmail.com>
2026-01-27 13:19:00 +0500

    Update sql_queries.sql

--- projects/billing-analysis/sql_queries.sql
+++ projects/billing-analysis/sql_queries.sql
@@ -1,3 +1,9 @@
 -- Operations & Billing Data Analysis
 -- Author: Muhammad Tayyab
 -- Dataset: billing_data.csv
+-- Q1: Customers per plan type
+SELECT
+  plan_type,
+  COUNT(*) AS customer_count
+FROM billing_data
+GROUP BY plan_type;
